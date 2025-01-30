6287120fef70fb5864841b0593124276af2dbd90
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2025-01-31 00:38:22 +0300

    Forms And Validation handled. (#2)

--- views/register.php
+++ views/register.php
@@ -1,16 +1,9 @@
 <h1>Register</h1>
-<form method="post" action="">
-  <div class="form-group">
-    <label for="name-field">Name</label>
-    <input type="text" class="form-control" id="name-field" aria-describedby="name-f" placeholder="Enter name" name="name">
-  </div>
-  <div class="form-group">
-    <label for="email-field">Email</label>
-    <input type="email" class="form-control" id="email-field" placeholder="email" name="email">
-  </div>
-  <div class="form-group">
-    <label for="password-field">Password</label>
-    <input type="password" class="form-control" id="password-field" placeholder="******" name="password">
-  </div>
+
+<?php $form = \app\core\form\Form::begin('', "post") ?>
+  <?php echo $form->field($model, 'name', "Name") ?>
+  <?php echo $form->field($model, 'email', "Email") ?>
+  <?php echo $form->field($model, 'password', "Password")->passwordField() ?>
+  <?php echo $form->field($model, 'passwordRepeat', "Repeat Password")->passwordField() ?>
   <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php echo \app\core\form\Form::end() ?>
