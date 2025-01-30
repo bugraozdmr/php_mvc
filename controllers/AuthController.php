6195f01c115961b40fa02bfb5447e54888433fd3
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2025-01-31 00:37:19 +0300

    Forms And Validation handled.

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,6 +3,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller
 {
@@ -14,10 +15,23 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $this->setLayout('auth');
-        if($request->isPost() === 'post') {
-            return 'Handling submitted data';
+        $registerModel = new RegisterModel();
+
+        if($request->isPost()) {
+            $registerModel->loadData($request->getBody());
+            
+            if($registerModel->validate() && $registerModel->register()) {
+                return 'Success';
+            }
+
+            return $this->render('register', [
+                'model' => $registerModel
+            ]);
         }
-        return $this->render('register');
+
+        $this->setLayout('auth');
+        return $this->render('register', [
+            'model' => $registerModel
+        ]);
     }
 }
---
+++ models/registerModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class RegisterModel extends Model
+{
+    // initilize the variables to empty strings (to avoid undefined variable error)
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $passwordRepeat = '';
+
+    public function register()
+    {
+        return 'Creating new user';
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 6], [self::RULE_MAX, 'max' => 24]],
+            'passwordRepeat' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+        ];
+    }
+}
